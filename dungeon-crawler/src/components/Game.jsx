7a8ed34a1e45c0e919ae32aeb50c09c32b9e12a2
Joni Sillanpää <jonimikaelsaari@gmail.com>
2025-04-11 15:45:10 +0300

    refactor: streamline player movement logic and enhance enemy AI behavior

--- dungeon-crawler/src/components/Game.jsx
+++ dungeon-crawler/src/components/Game.jsx
@@ -1,27 +1,3 @@
-/* import { React, useEffect } from 'react';
-import GameCanvas from './GameCanvas.jsx';
-import CharacterCanvas from './CharacterCanvas.jsx';
-import Player from './Player.jsx';
-import { startEnemyAI } from "../utils/enemyAI.js";
-import './Game.css';
-
-const Game = () => {
-
-  useEffect(() => {
-    startEnemyAI();
-  }, []);
-
-  return (
-    <div className="canvas-container">
-      <GameCanvas />
-      <CharacterCanvas />
-      <Player />
-    </div>
-  );
-};
-
-export default Game;
- */
 
 import React, { useState, useEffect } from 'react';
 import GameCanvas from './GameCanvas.jsx';
