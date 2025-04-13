3f5e274febb53cd593f9d79baad14af5f35c899a
Joni Sillanpää <jonimikaelsaari@gmail.com>
2025-04-13 12:42:54 +0300

    Kuolema grafiikkaa.

--- dungeon-crawler/src/components/GameOverOverlay.jsx
+++ dungeon-crawler/src/components/GameOverOverlay.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./GameOverOverlay.css";
+import deathImage from "../assets/spr_death.png";
+
 
 const GameOverOverlay = () => {
   return (
     <div className="game-over-overlay">
-      <h1 className="game-over-title">You Died</h1>
+      <img className="game-over-image" src={deathImage} alt="Game Over Image" />
       <p className="game-over-subtext">Press R to Restart</p>
     </div>
   );
