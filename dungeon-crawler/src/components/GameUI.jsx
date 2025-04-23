9fbf92c76922530291451303679bb026a9e1c351
Joona Nuorala <joona.nuorala@gmail.com>
2025-04-23 17:40:12 +0300

    feat: expand GameUI with player stats and progression info
    - Display attack, defense, XP to next level, and current dungeon level

--- dungeon-crawler/src/components/GameUI.jsx
+++ dungeon-crawler/src/components/GameUI.jsx
@@ -3,15 +3,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectPlayerStats } from "../redux/reducers/playerSlice";
+import { selectDungeonLevel } from "../redux/reducers/mapSlice";
 
 const PlayerStats = () => {
   const stats = useSelector(selectPlayerStats);
+  const dungeonLevel = useSelector(selectDungeonLevel);
 
   return (
     <div className="player-stats">
       <p>HP: {stats.hp} / {stats.maxHp}</p>
+      <p>Att: {stats.attack}</p>
+      <p>Def: {stats.defense}</p>
       <p>Exp: {stats.experience}</p>
+      <p>Exp required: {stats.experienceToNextLevel}</p>
       <p>Lvl: {stats.level}</p>
+      <p>Dungeon lvl: {dungeonLevel}</p>
     </div>
   );
 };
